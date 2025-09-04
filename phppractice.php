efb5744178507be8c91a211b72841201f421ba41
Jvst1ne <163095588+Jvst1ne@users.noreply.github.com>
2025-09-04 18:53:55 +0800

    Add files via upload

---
+++ phppractice.php
@@ -0,0 +1,120 @@
+<!-- Hello!
+<h1>Hello!</h1>
+<style>
+    h1 {
+        color: yellow;
+    }
+</style>
+><script>
+    alert("Hello!");
+</script> -->
+<?php
+    echo "<b>Hellow World!</b>";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello this is Me!</h1>
+    <?php
+        // echo "Hello";
+
+        // $color = "Red";
+        // $Color = "Blue";
+        // echo $Color;
+        // echo $color;
+
+        // $color_ = 5;
+
+        // echo $color_;
+
+        // var_dump("$color");
+        // var_dump([1,2]);
+
+
+        // variable scope
+        // $x = 5; //global
+
+        // function test(){
+        //     // $x = 10; //local
+        //     static $x = 10; //static
+        //     echo $x;
+        //     $x++;
+        //     echo "<br>";
+        // }
+
+        // test();
+        // test();
+        // test();
+        // test();
+        // test();
+
+        // echo $x;
+
+
+        // echo & print
+
+        // echo("Hello"), ("Hello");
+        // echo "World!", "World!";
+        // print("Hello");
+        // print "World!";
+
+        // $x = print("Hello");
+        // echo $x;
+
+
+        // $a = "Justine";
+        // $b = 5;
+        // $c = 4.1;
+        // $d = true;
+        // $e = [1,2,3];
+        // $f = new stdClass();
+        // $g = null;
+        // $h = fopen("php_practice.php", "r");
+
+        // var_dump($a);
+        // echo "<br>";
+        // var_dump($b);
+        // echo "<br>";
+        // var_dump($c);
+        // echo "<br>";
+        // var_dump($d);
+        // echo "<br>";
+        // var_dump($e);
+        // echo "<br>";
+        // var_dump($f);
+        // echo "<br>";
+        // var_dump($g);
+        // echo "<br>";
+        // var_dump($h);
+        // echo "<br>";
+
+        // $x = "Juan Cruz";
+        // echo str_replace("Cruz ", "Lance", $x);
+
+        // echo trim("Hello World");
+
+       // echo "Hello World!";
+
+       // $x = "Hello";
+       // $y = "Love";
+
+       // echo $x . " " . $y, "<br>";
+       // echo "$x $y";
+       // echo 'Juan "Immortal" Enrile';
+
+        $X =10;
+        DEFINE("GREETING", 10);
+
+        echo $x;
+        echo _GREETING;
+    ?>
+</body>
+</html>
+
+
